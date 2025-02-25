7046c46d6a902447e64118a6b4f93878e4f488c9
Shuang Wu <shuang111994@gmail.com>
2025-02-25 14:40:51 -0800

    modified block to create explosion force to push balls when destroyed

--- Assets/BuildingBlock.cs
+++ Assets/BuildingBlock.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class BuildingBlock : MonoBehaviour
 {
     public int blockType; // Type of block (1-6)
-    public int collisionThreshold = 5; // Number of hits before destruction
+    public int collisionThreshold = 4; // Number of hits before destruction
     private int collisionCount = 0; // Tracks collisions with the ball
+
+    public float explosionForce = 7f; // Strength of explosion
+    public float explosionRadius = 0.5f; // Radius of effect
+    public float upwardModifier = 1f; // Upward push effect
 
     void OnCollisionEnter(Collision collision)
     {
@@ -14,13 +18,31 @@ public class BuildingBlock : MonoBehaviour
 
             if (collisionCount >= collisionThreshold)
             {
+                Explode();
                 SelfDestruct();
+            }
+        }
+    }
+
+    void Explode()
+    {
+        Collider[] nearbyObjects = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (Collider obj in nearbyObjects)
+        {
+            if (obj.CompareTag("ball"))
+            {
+                Rigidbody rb = obj.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, upwardModifier, ForceMode.Impulse);
+                }
             }
         }
     }
 
     void SelfDestruct()
     {
-        Destroy(gameObject); // Destroys the block
+        Destroy(gameObject);
     }
 }
