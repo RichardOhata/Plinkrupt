a9fc02a6bc0f0973da48c3506c56a47b36b5f659
MMckenzie861 <namescsg@gmail.com>
2025-02-24 19:02:38 -0800

    Changed scoring and removed bidding

--- Assets/_Scripts/Ball/BallSpawner.cs
+++ Assets/_Scripts/Ball/BallSpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class BallSpawner : MonoBehaviour
 {
-    int numBalls = 1;
+    int numBalls = 10;
 
     public GameObject[] ballPrefab;
     private Button dropButton;
@@ -19,22 +19,19 @@ public class BallSpawner : MonoBehaviour
     {
         dropButton.interactable = false;
 
-        //place the bid
-        GameManager.Instance.PlaceBid(GameManager.Instance.currentBid * numBalls);
-
         StartCoroutine(BallSpawn());
     }
 
     IEnumerator BallSpawn()
     {
-        for (int i = 0; i < numBalls; i++)
+        for (int i = 0; i < 10; i++)
         {
             //instantiate ball object
             GameObject ball = Instantiate(ballPrefab[Random.Range(0, ballPrefab.Length)], transform.position, Quaternion.identity);
 
             //set the current bid value to the ball
             IElementScoreInteraction elementScoreInteraction = ball.GetComponent<IElementScoreInteraction>();
-            elementScoreInteraction.setCurrentBid(GameManager.Instance.currentBid);
+            elementScoreInteraction.setCurrentBid(100);
 
             yield return new WaitForSeconds(0.4f);
         }
---
+++ Assets/_Scripts/Scoring/ScoringWindow.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoringWindow : MonoBehaviour
+{
+    public GameObject panel; // Assign your UI panel in the Inspector
+    public int maxScores = 4; // Maximum number of displayed scores
+    private Queue<GameObject> scoreQueue = new Queue<GameObject>(); // Queue to manage score images
+    private float spacing = 120f; // Vertical spacing between score entries
+
+    public void AddScore(float score)
+    {
+        // If queue is full, remove the oldest image
+        if (scoreQueue.Count >= maxScores)
+        {
+            GameObject oldScore = scoreQueue.Dequeue();
+            Destroy(oldScore);
+        }
+
+        // Move existing images up
+        foreach (GameObject obj in scoreQueue)
+        {
+            RectTransform rt = obj.GetComponent<RectTransform>();
+            rt.anchoredPosition += new Vector2(0, spacing);
+        }
+
+        // Create a new blank white image for the score
+        GameObject scoreObject = new GameObject("ScoreEntry");
+        scoreObject.transform.SetParent(panel.transform, false);
+
+        // Add Image component and set it to white
+        Image img = scoreObject.AddComponent<Image>();
+        img.color = Color.white; // Set to white
+        RectTransform imgRect = img.rectTransform;
+        imgRect.sizeDelta = new Vector2(100, 100); // Set size
+        imgRect.anchorMin = new Vector2(0.5f, 1); // Center top
+        imgRect.anchorMax = new Vector2(0.5f, 1);
+        imgRect.pivot = new Vector2(0.5f, 1); // Align to top center
+        imgRect.anchoredPosition = new Vector2(0, -3 * spacing - 20); // Start at bottom
+
+        // Create TextMeshPro text inside the image
+        GameObject textObject = new GameObject("ScoreText");
+        textObject.transform.SetParent(scoreObject.transform, false);
+
+        TextMeshProUGUI tmp = textObject.AddComponent<TextMeshProUGUI>();
+        tmp.text = "Score: " + score;
+        tmp.fontSize = 24;
+        tmp.alignment = TextAlignmentOptions.Center;
+        tmp.color = Color.black; // Set text color to black for visibility
+
+        // Stretch text to fit inside the white image
+        RectTransform textRT = tmp.rectTransform;
+        textRT.anchorMin = new Vector2(0, 0);
+        textRT.anchorMax = new Vector2(1, 1);
+        textRT.offsetMin = Vector2.zero;
+        textRT.offsetMax = Vector2.zero;
+
+        // Add new score entry to queue
+        scoreQueue.Enqueue(scoreObject);
+    }
+}
