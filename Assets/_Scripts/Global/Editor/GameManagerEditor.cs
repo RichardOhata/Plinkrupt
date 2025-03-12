2a325ea79c73c0d2afeba8c7bf95e8e7ef02d99b
Andrew L. <andrewliang0123@gmail.com>
2025-03-12 14:38:34 -0700

    add save reset method to editor

---
+++ Assets/_Scripts/Global/Editor/GameManagerEditor.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+using UnityEngine;
+using UnityEditor;
+using Esper.ESave;
+
+[CustomEditor(typeof(GameManager))]
+public class GameManagerEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        
+        GameManager gameManager = (GameManager)target;
+        GameObject currentObject = gameManager.gameObject;
+        if (GUILayout.Button("Clear Money Save Data")) {
+            SaveFile saveFile = currentObject.GetComponent<SaveFileSetup>().GetSaveFile();
+            saveFile.DeleteData("Money");
+        }
+        if(GUILayout.Button("Clear All Save Data")) {
+            SaveFile saveFile = currentObject.GetComponent<SaveFileSetup>().GetSaveFile();
+            saveFile.EmptyFile();
+        }
+    }
+}
