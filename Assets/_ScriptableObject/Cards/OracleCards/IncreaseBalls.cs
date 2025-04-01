e6326718811d6a2ffb1b76ed90e1e522ec729a1d
RichardOhata <richardohata@gmail.com>
2025-03-31 23:51:04 -0700

    Added card that inceases # of balls

---
+++ Assets/_ScriptableObject/Cards/OracleCards/IncreaseBalls.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IncreaseBalls", menuName = "Scriptable Objects/Oracle Actions/IncreaseBalls")]
+public class IncreaseBalls : OraclePack
+{
+    private int ballIncAmt = 2;
+    public override void PerformAction()
+    {
+
+        GameManager.Instance.ballSpawner.SetNumBalls(GameManager.Instance.ballSpawner.numBalls + ballIncAmt);
+    }
+
+    public override string Description => $"Increase your total ball amount by 2 \n(Current Amount: {GameManager.Instance.ballSpawner.numBalls})";
+}
