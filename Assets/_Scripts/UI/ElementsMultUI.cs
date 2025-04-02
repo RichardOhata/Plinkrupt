b91554094cc98f18b2bfc7a2a13000ab12bc2d96
MMckenzie861 <namescsg@gmail.com>
2025-04-02 13:03:42 -0700

    Small fixes

--- Assets/_Scripts/UI/ElementsMultUI.cs
+++ Assets/_Scripts/UI/ElementsMultUI.cs
@@ -6,18 +6,18 @@ public class ElementsMultUI : MonoBehaviour
 {
     public ElementMultiplierManager EMM;
     private List<ElementMultiplier> multList;
+    private TMP_Text[] textComponents;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         multList = EMM.premanentMultipliers;
+        textComponents = GetComponentsInChildren<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-         TMP_Text[] textComponents = GetComponentsInChildren<TMP_Text>();
-
         for (int i = 0; i < multList.Count; i++)
         {
             textComponents[i].text = multList[i].multiplier.ToString();
